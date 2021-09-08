7e1a89b9cf90146380ee62b7a80b4e0ea2b8b317
ArtemAShch <79834403+ArtemAShch@users.noreply.github.com>
2021-09-08 23:45:51 +0300

    Add files via upload

---
+++ 1-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+	double a, b, c;
+	setlocale(LC_ALL, "Russian");
+	cout << "”равнение вида ах^2 + bx + c = 0 \n¬ведите коэффициент а: ";
+	cin>>a;
+	cout << "¬ведите коэффициент b: ";
+	cin>>b;
+	cout << "¬ведите коэффициент c: ";
+	cin>>c;
+	double D=b*b-(4*a*c);
+	if(D<0) cout<<"”равнение не имеет решений в действительных числах.";
+	else{
+		if(D==0){
+			cout<<"”равнение имеет одно решение: х = ";
+			cout<<-b/(2*a);
+		}
+		else{
+			cout<<"”равнение имеет два решени€: х1 = ";
+			cout<<(-b+pow(D, 0.5))/(2*a);
+			cout<<", х2 = ";
+			cout<<(-b-pow(D, 0.5))/(2*a);
+		}
+	}
+}
