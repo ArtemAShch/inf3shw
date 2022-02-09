41762d3ac698580142a13caa2cbf02711169cf96
Артём Щербаков <shcherbakov.artem@phystech.edu>
2022-02-10 00:43:27 +0300

    Вероятно будет ругаться, так как оператор = я написал отдельно. Его нет в chrono по умолчанию.

--- 2_1_1/main.cpp
+++ 2_1_1/main.cpp
@@ -15,12 +15,12 @@ public:
         std::cout<<"time is: "<<duration_cast<aboba>( steady_clock::now()- start).count()<<std::endl;
     }
     void Pause(){
-        buff = steady_clock::now();
-        std::cout<<"time is: "<<duration_cast<aboba>(steady_clock::now()- start).count()<<std::endl;
+        buff = duration_cast<aboba>(steady_clock::now()-start);
+        std::cout<<"time is: "<<buff.count()<<std::endl;
     }
     void Continue(){
-        start+=steady_clock::now()-buff;
-        std::cout<<"time is: "<<duration_cast<aboba>(steady_clock::now()- start).count()<<std::endl;
+        start = steady_clock::now()-buff;
+        std::cout<<"time is: "<<duration_cast<aboba>( steady_clock::now()- start).count()<<std::endl;
     }
     void Stop(){
 
@@ -30,23 +30,24 @@ public:
     }
 private:
     steady_clock::time_point start;
-    steady_clock::time_point buff;
+    duration<int, std::micro> buff;
 };
 
 int main() {
-    Timer <milliseconds>t ;
-    t.Pause();
-    t.Continue();
-    t.Reset();
+    Timer <microseconds>t ;
+
+
+  //  ;
+//  t.Reset();
     auto result = 0.;
     for (auto i = 0u; i<1000000u; i++){
         result += sin(i)*cos(i);
     }
-
+    t.Pause();
     for (auto i = 0u; i<1000000u; i++){
         result += sin(i)*cos(i);
     }
-
+    t.Continue();
     std::cout<< result<<std::endl ;
 
     return 0;
